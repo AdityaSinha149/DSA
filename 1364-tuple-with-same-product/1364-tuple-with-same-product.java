f88af66492cdfc0d8c7386b65001ce1bfc478c3e
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-02-06 16:18:28 +0530

    Time: 163 ms (96.15%), Space: 68.6 MB (69.23%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -1,17 +1,20 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        Map<Integer,Integer> multToTimesMap=new HashMap<>();
-        for(int n:nums)
-            for(int m:nums)
-                multToTimesMap.put(n*m,multToTimesMap.getOrDefault(n*m, 0)+1);
-
-        int cnt=0;
-        for(int value:multToTimesMap.values())
-            cnt+=8*nC2(value/2);
+        Map<Integer, Integer> multToTimesMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                multToTimesMap.put(nums[i] * nums[j], multToTimesMap.getOrDefault(nums[i] * nums[j], 0) + 1);
+            }
+        }
+        
+        int cnt = 0;
+        for (int value : multToTimesMap.values()) {
+            cnt += 8 * nC2(value);
+        }
         return cnt;
     }
 
-    private int nC2(int n){
-        return n*(n-1)/2;
+    private int nC2(int n) {
+        return n * (n - 1) / 2;
     }
 }

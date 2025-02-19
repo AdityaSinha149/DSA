33a3bfa9f7739a836b3d6024bb5b0bf089999c98
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-02-19 19:29:24 +0530

    Time: 3 ms (73.35%), Space: 42.3 MB (70.84%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,11 +1,12 @@
 class Solution {
     public String getHappyString(int n, int k) {
         List<String> list = new ArrayList<>();
-        generateHappy(new StringBuilder(), n, list);
+        generateHappy(new StringBuilder(), n,k, list);
         return k <= list.size() ? list.get(k - 1) : "";
     }
 
-    private void generateHappy(StringBuilder sb, int n, List<String> list) {
+    private void generateHappy(StringBuilder sb, int n,int k, List<String> list) {
+        if(list.size()==k)return;
         if (sb.length() == n) {
             list.add(sb.toString());
             return;
@@ -14,7 +15,7 @@ class Solution {
         for (char c : new char[]{'a', 'b', 'c'}) {
             if (sb.length() == 0 || sb.charAt(sb.length() - 1) != c) {
                 sb.append(c);
-                generateHappy(sb, n, list);
+                generateHappy(sb, n,k, list);
                 sb.deleteCharAt(sb.length() - 1);
             }
         }

00a14228a9258f13de225e633a295101a321a66f
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-03-05 20:08:52 +0530

    Time: 0 ms (100%), Space: 40.4 MB (59.02%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,11 +1,7 @@
 class Solution {
-    public long coloredCells(int n) {
-        long num=1;
-        int four=4;
-        for(int i=2;i<=n;i++){
-            num+=four;
-            four+=4;
-        }
-        return num;
+    public long coloredCells(int m) {
+        long n=m;
+        n--;
+        return 1+2*(n*n+n);
     }
 }

05cc86d4f98a69f024191572e7a36fc5f29ef427
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-03-17 01:44:58 +0530

    Time: 46 ms (57.78%), Space: 56.5 MB (32.72%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -0,0 +1,25 @@
+class Solution {
+    private boolean possible(int[] ranks, int cars, long target){
+        long c=0;
+        for(int r:ranks){
+            long part=(long)Math.sqrt(target/r);
+            c+=part;
+        }
+        return c>=cars;
+    }
+    public long repairCars(int[] ranks, int cars) {
+        long l=0;
+        int min=ranks[0];
+        for(int r:ranks)
+            min=Math.min(min, r);
+        long h=1L*min*cars*cars;
+
+        while(l<=h){
+            long m=l+(h-l)/2;
+            if(!possible(ranks, cars, m)) l=m+1;
+            else h=m-1;
+        }
+
+        return h+1;
+    }
+}

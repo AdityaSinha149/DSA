a18c8988f3abbaca4cad776c17e98fbff0826a81
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-06-06 03:26:30 +0530

    Time: 3 ms (79.9%), Space: 44.5 MB (96.67%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,30 @@
 class Solution {
-    static Map<Character, int[]> romanMap = new HashMap<>();
-    static{
-
-        romanMap.put('I', new int[]{1, 1});
-        romanMap.put('V', new int[]{2, 5});
-        romanMap.put('X', new int[]{3, 10});
-        romanMap.put('L', new int[]{4, 50});
-        romanMap.put('C', new int[]{5, 100});
-        romanMap.put('D', new int[]{6, 500});
-        romanMap.put('M', new int[]{7, 1000});
+    static Map<Character, Integer> values = new HashMap<>();
+    static {
+        values.put('M', 1000);
+        values.put('D', 500);
+        values.put('C', 100);
+        values.put('L', 50);
+        values.put('X', 10);
+        values.put('V', 5);
+        values.put('I', 1);
     }
+
     public int romanToInt(String s) {
-        int[] first=romanMap.get(s.charAt(s.length()-1));
-        int val=first[1];
-        int prevPrior=first[0];
-        for(int i=s.length()-2;i>=0;i--){
-            int[] curr=romanMap.get(s.charAt(i));
-            if(curr[0]>=prevPrior)val+=curr[1];
-            else val-=curr[1];
-            prevPrior=curr[0];
+        Character lastSymbol = s.charAt(s.length()-1);
+        int lastValue = values.get(lastSymbol);
+        int total = lastValue;
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            Character currentSymbol = s.charAt(i);
+            int currentValue = values.get(currentSymbol);
+            if (currentValue < lastValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            lastValue = currentValue;
         }
-        return val;
+        return total;
     }
 }
